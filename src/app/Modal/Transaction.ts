2009f3fea2ac5fb5eca8e3803ece03564b47ecbb
Pramit316 <primeshipbas@gmail.com>
2026-03-23 18:42:33 +0545

    added form and submission to local storage

---
+++ src/app/Modal/Transaction.ts
@@ -0,0 +1,9 @@
+export interface Transaction {
+  title: string,
+  amount: number,
+  transactionType: string,
+  paymentMethod: string,
+  category: string
+  date: string,
+  desctiption: string
+}
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,21 +1,47 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Transaction} from '../../Modal/Transaction';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
+  private readonly storageKey = "formStorage";
+  private transactions: Transaction[] = [];
+
   transactionForm: FormGroup = new FormGroup({
     title: new FormControl('', Validators.required),
     amount: new FormControl(null, Validators.required),
-    type: new FormControl('',),
-    income: new FormControl(''),
+    transactionType: new FormControl('',),
+    paymentMethod: new FormControl(''),
     category: new FormControl(''),
+    date: new FormControl('', Validators.required),
+    desctiption: new FormControl('')
   });
 
+  onSubmit() {
+    const newForm = this.transactionForm.getRawValue();
+
+    this.transactions.push(newForm);
+    localStorage.setItem(this.storageKey, JSON.stringify(newForm));
+
+    console.log(this.transactions);
+
+    this.transactionForm.reset({
+      title: '',
+      amount: null,
+      paymentMethod: '',
+      transactionType: 'expense',
+      category: '',
+      date: '',
+      description: ''
+    });
+  }
 
 }
